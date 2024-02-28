b580aa0a009158bc4df865c5399da708f3957e50
Michael Lombardi <mikelombardi4332@gmail.com>
2024-02-27 22:03:39 -0600

    First commit, Refactored step 1

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,55 +1,47 @@
 class GildedRose
   attr_reader :name, :days_remaining, :quality
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+  def initialize(name, quality, days_remaining)
+    @name,@quality,@days_remaining = name, quality, days_remaining
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
+    case name
+    when 'normal'
+      normal tick
+    when 'Aged Brie'
+        brie_tick
+    when 'Sulfaras, Hand of Ragnaros'
+      sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+        backstage_tick
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+  end
+  def normal_tick
+    @days_remaining -= 1
+    return if @quality == 0
+
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
+  end 
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end 
+
+  def sulfuras_tick
+  end
+  
+  def backstage_tick
+    @days_remaining -=1
+    return  if @quality >= 50 
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
   end
 end
