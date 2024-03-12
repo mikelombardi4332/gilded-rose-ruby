858a8feda82529abab5984eb8d4004234f9c8161
Michael Lombardi <mikelombardi4332@gmail.com>
2024-03-12 17:37:24 -0500

    Resubmit, second commit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,47 +1,67 @@
-class GildedRose
-  attr_reader :name, :days_remaining, :quality
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
 
-  def initialize(name, quality, days_remaining)
-    @name,@quality,@days_remaining = name, quality, days_remaining
-  end
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
 
-  def tick
-    case name
-    when 'normal'
-      normal tick
-    when 'Aged Brie'
-        brie_tick
-    when 'Sulfaras, Hand of Ragnaros'
-      sulfuras_tick
-    when 'Backstage passes to a TAFKAL80ETC concert'
-        backstage_tick
+    def tick
+
     end
   end
-  def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
 
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
-  end 
-  def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
-  end 
-
-  def sulfuras_tick
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
-  
-  def backstage_tick
-    @days_remaining -=1
-    return  if @quality >= 50 
-    return @quality = 0 if @days_remaining < 0
 
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured}
+
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
